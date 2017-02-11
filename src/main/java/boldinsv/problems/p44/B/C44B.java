21f1cc4ba7cdb1b97f7cf79bc999600eb03cb934
Sergey Toropov <vivat.toropovsi@gmail.com>
2017-02-11 20:54:34 +1000

    The task 44B is complated!

--- src/main/java/boldinsv/problems/p44/B/C44B.java
+++ src/main/java/boldinsv/problems/p44/B/C44B.java
@@ -3,9 +3,9 @@ package boldinsv.problems.p44.B;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.stream.IntStream;
 
 public class C44B {
-
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String[] items = reader.readLine().split(" ");
@@ -14,17 +14,20 @@ public class C44B {
         int a = Integer.parseInt(items[1]);
         int b = Integer.parseInt(items[2]);
         int c = Integer.parseInt(items[3]);
-        int answer = 0;
 
-        for (int bottle2l = 0; bottle2l <= c; bottle2l++) {
-            for (int bottle1l = 0; bottle1l <= b; bottle1l++) {
-                int bottle05l = (amountL - (bottle2l * 2) - bottle1l) * 2;
-
-                if (bottle05l >= 0 && bottle05l <= a) {
-                    answer++;
-                }
-            }
-        }
+        int answer = IntStream
+                .rangeClosed(0, c)
+                .map(bottle2l -> {
+                    return IntStream
+                            .rangeClosed(0, b)
+                            .filter(bottle1l -> {
+                                int bottle05l = (amountL - (bottle2l * 2) - bottle1l) * 2;
+                                return bottle05l >= 0 && bottle05l <= a;
+                            })
+                            .map(bottle1l -> 1)
+                            .sum();
+                })
+                .sum();
 
         System.out.println("" + answer);
     }
