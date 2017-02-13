8555877653d71d23e3b8e075e12035f6eec03a4e
Sergey Toropov <vivat.toropovsi@gmail.com>
2017-02-13 22:29:45 +1000

    The task 304A is completed!

--- src/main/java/boldinsv/problems/p304/A/C304A.java
+++ src/main/java/boldinsv/problems/p304/A/C304A.java
@@ -3,6 +3,7 @@ package boldinsv.problems.p304.A;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.stream.IntStream;
 
 public class C304A {
 	public static void main(String[] args) throws IOException {
@@ -10,22 +11,24 @@ public class C304A {
 		int n = Integer.parseInt(reader.readLine());
 		reader.close();
 
-		int answer = 0;
-		for (int a = 1; a <= n; a++) {
-			for (int b = a; b <= n; b++) {
+		int answer = IntStream
+				.rangeClosed(1, n)
+				.map(a ->
+					IntStream
+							.rangeClosed(a, n)
+							.filter(b -> {
+								int sum = a * a + b * b;
+								int sqrt = (int) Math.sqrt(sum);
 
-				int sum = a * a + b * b;
-				int sqrt = (int) Math.sqrt(sum);
+								if (sqrt > n) {
+									return false;
+								}
+								return sqrt * sqrt == sum;
+							})
+							.map(b -> 1)
+							.sum())
+				.sum();
 
-				if (sqrt > n) {
-					break;
-				}
-
-				if (sqrt * sqrt == sum) {
-					answer += 1;
-				}
-			}
-		}
 		System.out.println("" + answer);
 	}
 }
--- src/main/java/boldinsv/problems/p381/A/C381A.java
+++ src/main/java/boldinsv/problems/p381/A/C381A.java
@@ -3,27 +3,26 @@ package boldinsv.problems.p381.A;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.stream.IntStream;
 
 public class C381A {
     public static int n;
     public static Deque<Integer> numbers;
-    public static int sumSergey;
-    public static int sumDima;
+    public static Result result;
 
     public static void init() {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] items;
 
         try {
             n = Integer.parseInt(reader.readLine());
-            items = reader.readLine().split(" ");
 
-            numbers = new LinkedList<Integer>();
-            for (String item: items) {
-                numbers.add(Integer.parseInt(item));
-            }
+            numbers = new LinkedList<>();
+            Arrays.asList(reader.readLine().split(" ")).stream()
+                    .map(Integer::parseInt)
+                    .forEach(value -> numbers.add(value));
 
             reader.close();
         } catch (IOException ioe) {
@@ -31,34 +30,43 @@ public class C381A {
         }
     }
 
-    public static void run() {
-        final int SERGEY = 0;
-        final int DIMA   = 1;
-        int state = SERGEY;
+    public static class Result {
+        public enum Gamers {SERGEY, DIMA};
+        public Gamers activeGamer = Gamers.SERGEY;
 
-        sumSergey = 0;
-        sumDima   = 0;
-        while (numbers.size() > 0) {
-            int max = numbers.peekFirst() > numbers.peekLast() ? numbers.pollFirst() : numbers.pollLast();
+        public int max = 0;
+        public int sumSergey = 0;
+        public int sumDima = 0;
 
-            switch (state) {
-                case SERGEY:
-                    state = DIMA;
+        public Result() {}
 
-                    sumSergey += max;
-                    break;
-
-                case DIMA:
-                    state = SERGEY;
-
-                    sumDima += max;
-                    break;
-            }
+        public Result(Integer max) {
+            this.max = max;
         }
     }
 
+    public static void run() {
+        result = IntStream
+                .range(0, numbers.size())
+                .mapToObj(index -> new Result(numbers.peekFirst() > numbers.peekLast() ? numbers.pollFirst() : numbers.pollLast()))
+                .reduce(new Result(), (result, element) -> {
+                    switch (result.activeGamer) {
+                        case SERGEY:
+                            result.activeGamer = Result.Gamers.DIMA;
+                            result.sumSergey += element.max;
+                            break;
+
+                        case DIMA:
+                            result.activeGamer = Result.Gamers.SERGEY;
+                            result.sumDima += element.max;
+                            break;
+                    }
+                    return result;
+                });
+    }
+
     public static void print() {
-        System.out.println(sumSergey + " " + sumDima);
+        System.out.println(result.sumSergey + " " + result.sumDima);
     }
 
     public static void main(String[] args) {
